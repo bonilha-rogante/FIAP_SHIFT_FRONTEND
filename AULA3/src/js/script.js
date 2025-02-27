295d12048370c23e4cd32aab5a5193c3b04c8ad5
Bonilha <bonilha.rogante@gmail.com>
2025-02-27 20:01:44 -0300

    Declarando variáveis | tipos | Var Let Const

---
+++ AULA3/src/js/script.js
@@ -0,0 +1,64 @@
+// TIPOS DE VARIÁVEIS
+
+var exemplo = "Olá, Dev-var!";
+console.log(exemplo);
+
+let exemplo1 = "Olá, Dev-let!";
+console.log(exemplo1)
+
+const exemplo2 = "Olá, Dev-const";
+console.log(exemplo2)
+
+let exemplo3="10";
+console.log(exemplo3)
+
+// VAR
+function exemplo4(){
+    if(true){
+        var f=100;
+        console.log("Dentro do bloco if: ", f)
+    }
+    console.log("Dentro do bloco função: ". f)
+}
+
+exemplo4()
+
+// LET
+function exemplo5(){
+    if(true){
+        let z=200;
+        console.log("Dentro do if: ", z)
+    }
+    // console.log("Fora do if: ", z)
+}
+
+// CONST
+
+const nome = 'Fiap'
+// nome = 'Cidade'
+console.log(nome)
+
+
+let exemplo6=10
+console.log(typeof exemplo6)
+
+let exemplo7='10'
+console.log(typeof exemplo7)
+
+let exemplo8 = true
+console.log(typeof exemplo8)
+
+let objeto = {}
+console.log(typeof objeto)
+
+let array=[]
+console.log(typeof array)
+
+// Valor undefined
+let exemplo9;
+console.log(exemplo9)
+
+// Valor vazio
+let exemplo10 = null
+console.log(exemplo10)
+
